0d20499eb468147a9eddded115ecb5325b81b03d
CHUNDOOIN <chun.doo.in@gmail.com>
2022-05-11 15:44:30 +0900

    [feat] git 세팅

---
+++ src/components/units/product/list/ProductList.Container.tsx
@@ -0,0 +1,5 @@
+import ProductListPresenter from "./ProductList.Presenter";
+
+export default function ProductListContainer() {
+  return <ProductListPresenter></ProductListPresenter>;
+}
---
+++ src/components/units/product/list/ProductList.styles.ts
@@ -0,0 +1,121 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 1920px;
+
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  background-color: #ffffff;
+`;
+
+export const SearchWrapper = styled.div`
+  width: 100%;
+  padding: 79px 210px 30px 210px;
+
+  /* background-color: red; */
+
+  border-bottom: 2px solid #c4c4c4;
+`;
+
+export const SearchInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: space-around; */
+  align-items: center;
+`;
+
+export const SearchInput = styled.input`
+  width: 1500px;
+  height: 134px;
+
+  padding: 20px;
+
+  font-weight: 400;
+  font-size: 20px;
+
+  background-color: #c4c4c4;
+
+  margin-bottom: 83px;
+`;
+
+export const SearchTextWrapper = styled.div``;
+
+export const SearchText = styled.div`
+  width: 1500px;
+  font-weight: 400;
+  font-size: 24px;
+`;
+
+export const ProductListWrapper = styled.div`
+  width: 100%;
+  padding: 70px 210px 500px 210px;
+
+  /* display: flex; */
+  /* flex-direction: column; */
+`;
+
+export const ProductBoxWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  /* margin-bottom: 40px; */
+
+  flex-wrap: wrap;
+`;
+
+export const ProductBox = styled.div`
+  width: 730px;
+  height: 366px;
+
+  /* background: yellow; */
+  display: flex;
+  flex-direction: row;
+
+  background-color: #c4c4c4;
+
+  margin-bottom: 40px;
+`;
+
+export const ProductImage = styled.img`
+  width: 366px;
+  height: 366px;
+
+  /* background-image: url("/images/keyboard-01.jpg"); */
+  background-position: center;
+  background-size: cover;
+`;
+
+export const ProductBoxRightWrapper = styled.div`
+  width: 364px;
+  height: 366px;
+  display: flex;
+  flex-direction: column;
+
+  padding: 90px 30px 0px 40px;
+`;
+
+export const ProductTitle = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+
+  margin-bottom: 40px;
+`;
+
+export const ProductPrice = styled.div`
+  font-weight: 700;
+  font-size: 32px;
+
+  margin-bottom: 24px;
+`;
+
+export const ProductContents = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
