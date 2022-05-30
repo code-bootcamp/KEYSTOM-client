1a9b75028a4cee12ae0cfec2b38ba2d05f2be5e7
wjwow007 <eeeeve007@gmail.com>
2022-05-30 11:27:23 +0900

    [feat] 우정 관리자페이지

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import LayoutFooter from "./footer";
 import LayoutHeader from "./header";
 import styled from "@emotion/styled";
-// import LayoutAdminSideBar from "./admin/index";
+import LayoutAdminSideBar from "./admin/index";
 
 interface ILayoutProps {
   children: ReactNode;
@@ -29,29 +29,30 @@ const Body = styled.div`
   height: auto;
 `;
 
-// const Inner = styled.div`
-//   width: 100%;
-//   display: flex;
-// `;
+const Inner = styled.div`
+  width: 100%;
+  display: flex;
+`;
 
-const HIDDEN_HEADER = ["/"];
-// const HIDDEN_ADMIN_SIDEBAR = ["/", "/store", "/event", "/mypage", "product"];
+// const HIDDEN_HEADER = ["/"];
+const HIDDEN_ADMIN_SIDEBAR = ["/", "/store", "/event", "/mypage", "/product"];
 
 export default function Layout(props: ILayoutProps) {
   const router = useRouter();
 
-  let isHidden = HIDDEN_HEADER.includes(router.asPath);
-  // let sideIsHidden = HIDDEN_ADMIN_SIDEBAR.includes(router.asPath);
+  // isHidden = HIDDEN_HEADER.includes(router.asPath);
+      let isHidden = HIDDEN_ADMIN_SIDEBAR.includes(router.asPath);
 
   return (
     <Wrapper>
-      {!isHidden && <LayoutHeader />}
-      {/* <Inner> */}
-      {/* {!isHidden && !sideIsHidden && <LayoutAdminSideBar />} */}
+      {/* {!isHidden && <LayoutHeader />} */}
+      <LayoutHeader />
+      <Inner>
+      {!isHidden && <LayoutAdminSideBar />}
       <BodyWrapper>
         <Body>{props.children}</Body>
       </BodyWrapper>
-      {/* </Inner> */}
+      </Inner>
       <LayoutFooter />
     </Wrapper>
   );
