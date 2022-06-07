4fc5569bef868ae3b906a2285824c5564a434cf1
CHUNDOOIN <chun.doo.in@gmail.com>
2022-06-07 12:26:47 +0900

    [fix] 두인 배포2

--- src/components/units/review/ReviewWrite.types.ts
+++ src/components/units/review/ReviewWrite.types.ts
@@ -1,11 +1,13 @@
-import { ChangeEvent } from 'react';
-export interface IReviewWritePresenter{
-    onChangeReviewTitle:(event: ChangeEvent<HTMLInputElement>) => void
-    onChangeReviewDescription:(event: ChangeEvent<HTMLTextAreaElement>) => void
-    onClickSubmit: () => void
-    description:string
-    setImageUrls:any
-    imageUrls:any
-    data:any
-
+import { ChangeEvent } from "react";
+export interface IReviewWritePresenter {
+    onChangeReviewTitle: (event: ChangeEvent<HTMLInputElement>) => void;
+    onChangeReviewDescription: (
+        event: ChangeEvent<HTMLTextAreaElement>
+    ) => void;
+    onClickSubmit: () => void;
+    description: string;
+    setImageUrls: any;
+    imageUrls: any;
+    data: any;
+    customData: any;
 }
